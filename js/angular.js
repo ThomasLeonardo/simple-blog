896846d023fc82915b76a6711bc921ed87418a0e
thomas leonardo <thomas leonardo>
2017-06-12 13:58:17 -0300

    Login working in Controller

--- js/angular.js
+++ js/angular.js
@@ -1,13 +1,31 @@
 var app = angular.module('blog', []);
 
-app.controller('commentsController', function(){
+app.controller('commentController', function(){
 
 });
 
-app.controller('postsController', function(){
+app.controller('postController', function(){
 
 });
 
-app.controller('usersController', function(){
+app.controller('userController', function($scope, $http){
+	this.url = 'http://jsonplaceholder.typicode.com/users';
+	this.valid = false;
+	this.loggedUser = {};
+	$scope.email = "";
+	this.submitForm = function(){
+		$http.get(this.url).then(function(response){
+			console.log(response.data);
+			response.data.forEach(function(responseUser){
+				console.log(responseUser.email);
+				console.log($scope.email);
+				if($scope.email == responseUser.email){
+					this.loggedUser = responseUser;
+					this.valid = true;
+				}
+			});
+			console.log(this.loggedUser);
+		});
+	};
 
 });
