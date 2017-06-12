038251bb16d5306b2adcca20de05ef13611d13da
thomas leonardo <thomas leonardo>
2017-06-11 23:44:27 -0300

    Added basic angular wireframe

---
+++ js/angular.js
@@ -0,0 +1,13 @@
+var app = angular.module('blog', []);
+
+app.controller('commentsController', function(){
+	
+});
+
+app.controller('postsController', function(){
+
+});
+
+app.controller('usuersController', function(){
+
+});
